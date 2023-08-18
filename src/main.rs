9c3c676216fa8575d0d61886dea3d853687e64c1
Takeru Ohta <phjgt308@gmail.com>
2023-08-18 21:25:29 +0900

    Update clapt to v4

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate clap;
 extern crate dg;
 extern crate fibers;
@@ -10,7 +9,9 @@ extern crate sloggers;
 #[macro_use]
 extern crate trackable;
 
-use clap::{App, Arg, ArgMatches, SubCommand};
+use std::path::PathBuf;
+
+use clap::Parser;
 use dg::{agent, watch};
 use fibers::{Executor, InPlaceExecutor, Spawn};
 use futures::{Future, Stream};
@@ -18,50 +19,31 @@ use slog::Logger;
 use sloggers::types::Severity;
 use sloggers::Build;
 
+#[derive(Parser)]
+enum Args {
+    Watch { dir: PathBuf },
+    Agent { dir: PathBuf },
+}
+
 fn main() {
-    let matches = app_from_crate!()
-        .arg(
-            Arg::with_name("LOG_LEVEL")
-                .long("log-level")
-                .takes_value(true)
-                .possible_values(&["debug", "info", "warning", "error", "critical"])
-                .default_value("info"),
-        )
-        .subcommand(subcommand_watch())
-        .subcommand(subcommand_agent())
-        .get_matches();
+    let args = Args::parse();
+    let log_level = Severity::Debug;
+    let logger = track_try_unwrap!(sloggers::terminal::TerminalLoggerBuilder::new()
+        .level(log_level)
+        .destination(sloggers::terminal::Destination::Stderr)
+        .build());
 
-    let log_level = match matches.value_of("LOG_LEVEL").unwrap() {
-        "debug" => Severity::Debug,
-        "info" => Severity::Info,
-        "warning" => Severity::Warning,
-        "error" => Severity::Error,
-        "critical" => Severity::Critical,
-        _ => unreachable!(),
-    };
-    let logger = track_try_unwrap!(
-        sloggers::terminal::TerminalLoggerBuilder::new()
-            .level(log_level)
-            .destination(sloggers::terminal::Destination::Stderr)
-            .build()
-    );
-
-    if let Some(matches) = matches.subcommand_matches("watch") {
-        handle_watch(matches, logger);
-    } else if let Some(matches) = matches.subcommand_matches("agent") {
-        handle_agent(matches, logger);
-    } else {
-        println!("Usage: {}", matches.usage());
-        std::process::exit(1);
+    match args {
+        Args::Watch { dir } => {
+            handle_watch(dir, logger);
+        }
+        Args::Agent { dir } => {
+            handle_agent(dir, logger);
+        }
     }
 }
 
-fn subcommand_watch() -> App<'static, 'static> {
-    SubCommand::with_name("watch").arg(Arg::with_name("DIR").index(1).required(true))
-}
-
-fn handle_watch(matches: &ArgMatches, logger: Logger) {
-    let dir = matches.value_of("DIR").unwrap();
+fn handle_watch(dir: PathBuf, logger: Logger) {
     let executor = InPlaceExecutor::new().unwrap();
     let mut watcher = watch::fs::FileSystemWatcher::new(logger.clone(), executor.handle());
     track_try_unwrap!(watcher.watch(dir));
@@ -71,16 +53,19 @@ fn handle_watch(matches: &ArgMatches, logger: Logger) {
             .for_each(move |file| {
                 let logger = logger.new(o!("file" => format!("{:?}", file.path())));
                 let error_logger = logger.clone();
-                handle.spawn(file.for_each(move |content| {
-                    info!(
-                        logger,
-                        "Updated: position={}, bytes={}, eof={}",
-                        content.offset,
-                        content.data.len(),
-                        content.eof
-                    );
-                    Ok(())
-                }).map_err(move |e| error!(error_logger, "{}", e)));
+                handle.spawn(
+                    file.for_each(move |content| {
+                        info!(
+                            logger,
+                            "Updated: position={}, bytes={}, eof={}",
+                            content.offset,
+                            content.data.len(),
+                            content.eof
+                        );
+                        Ok(())
+                    })
+                    .map_err(move |e| error!(error_logger, "{}", e)),
+                );
                 Ok(())
             })
             .then(|_| Ok(())),
@@ -88,12 +73,7 @@ fn handle_watch(matches: &ArgMatches, logger: Logger) {
     executor.run().unwrap();
 }
 
-fn subcommand_agent() -> App<'static, 'static> {
-    SubCommand::with_name("agent").arg(Arg::with_name("DIR").index(1).required(true))
-}
-
-fn handle_agent(matches: &ArgMatches, logger: Logger) {
-    let dir = matches.value_of("DIR").unwrap();
+fn handle_agent(dir: PathBuf, logger: Logger) {
     let executor = InPlaceExecutor::new().unwrap();
     let mut watcher = watch::fs::FileSystemWatcher::new(logger.clone(), executor.handle());
     track_try_unwrap!(watcher.watch(dir));
